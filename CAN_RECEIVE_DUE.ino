1c3d916d193e7bad470a98a9409bd7e0004e87c3
anirudh666 <39361351+anirudh666@users.noreply.github.com>
2018-06-29 20:32:32 +0530

    Add files via upload

---
+++ CAN_RECEIVE_DUE.ino
@@ -0,0 +1,28 @@
+#include<variant.h>
+#include<due_can.h>
+void setup() 
+{
+   Serial.begin(9600);
+
+    Serial.println("---CAN Receiver---");
+    
+}
+void loop()
+{ CAN_FRAME msg;
+  Can0.begin(CAN_BPS_250K);
+ //Can0.watchFor(0x5A1);
+  while(Can0.available()==0);
+  Can0.read(msg);
+  Serial.println(msg.id);
+  Serial.println(msg.data.byte[0]);
+  Serial.println(msg.data.byte[1]);
+  Serial.println(msg.data.byte[2]);
+  Serial.println(msg.data.byte[3]);
+  Serial.println(msg.data.byte[4]);
+  Serial.println(msg.data.byte[5]);
+ Serial.println(msg.data.byte[6]);
+  Serial.println(msg.data.byte[7]);
+ 
+  delay(1000);
+}
+
---
+++ CAN_SEND_DUE.ino
@@ -0,0 +1,35 @@
+#include<due_can.h>
+#include"variant.h"
+CAN_FRAME msg;
+void setup() 
+{
+  
+  Serial.begin(9600);
+  Serial.println("---- CAN Sender ----");
+}
+
+void sender()
+{
+  msg.id=0x5A1;
+  msg.extended=LOW;
+  msg.priority=LOW;
+  msg.length=8;
+  msg.rtr=LOW; 
+  msg.data.byte[0]=0x0;
+  msg.data.byte[1]=0x1;
+  msg.data.byte[2]=0x2;
+  msg.data.byte[3]=0x3;
+  msg.data.byte[4]=0x4;
+  msg.data.byte[5]=0x5;
+  msg.data.byte[6]=0x6;
+  msg.data.byte[7]=0x7;
+}
+void loop() 
+{  
+  Can0.begin(CAN_BPS_250K);
+  sender();
+  if(CAN.sendFrame(msg))
+  Serial.println("Sent...........");
+  else Serial.println("CAN Failed!!!!");
+}
+
